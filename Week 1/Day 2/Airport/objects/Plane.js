839940d3e87949abf7b6503603dba4b8d999b732
LForchini <leonardo.forchini@gmail.com>
2022-01-05 13:42:14 +0000

    Added a test that uses mocks

--- Week 1/Day 2/Airport/objects/Plane.js
+++ Week 1/Day 2/Airport/objects/Plane.js
@@ -1,12 +1,20 @@
+const Bag = require("./Bag");
+
 /**
  * Represents a plane.
  * @constructor
  * @param {string} type The type of plane.
  */
 class Plane {
-  constructor(type) {
+  constructor(
+    type,
+    goodybag = () => {
+      new Bag(1);
+    }
+  ) {
     this.type = type;
     this.passengers = [];
+    this.goodybag = goodybag;
   }
 
   /**
@@ -15,6 +23,9 @@ class Plane {
    */
   board(passenger) {
     this.passengers.push(passenger);
+
+    // Each passenger gets a free goody bag
+    passenger.addBag(this.goodybag());
   }
 }
 
--- Week 1/Day 2/Airport/objects/Plane.test.js
+++ Week 1/Day 2/Airport/objects/Plane.test.js
@@ -1,3 +1,4 @@
+const Bag = require("./Bag");
 const Passenger = require("./Passenger");
 const Plane = require("./Plane");
 
@@ -13,5 +14,15 @@ describe("Plane", () => {
     plane.board(passenger);
     expect(plane.passengers.length).toBe(1);
     expect(plane.passengers[0]).toBe(passenger);
+  });
+
+  test("Successfully give the passenger a goodybag", () => {
+    const goodybagMock = jest.fn(() => {
+      new Bag(1);
+    });
+    const plane = new Plane("Type", goodybagMock);
+    const passenger = new Passenger("Name", "Passport Number", "Seat Number");
+    plane.board(passenger);
+    expect(goodybagMock.mock.calls.length).toBe(1);
   });
 });
