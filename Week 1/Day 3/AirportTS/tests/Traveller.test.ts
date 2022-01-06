5296e60ac5099cbfee4703a6efc41b97b3f32072
LForchini <leonardo.forchini@gmail.com>
2022-01-06 10:16:58 +0000

    Added tests for traveller class

---
+++ Week 1/Day 3/AirportTS/tests/Traveller.test.ts
@@ -0,0 +1,17 @@
+import Bag from "../src/Bag";
+import Traveller from "../src/Traveller";
+
+describe("Traveller", () => {
+  test("is created successfully", () => {
+    const traveller = new Traveller("Name");
+    expect(traveller.name).toBe("Name");
+  });
+
+  test("can add bags", () => {
+    const traveller = new Traveller("Name");
+    const bag = new Bag(10);
+    traveller.addBag(bag);
+    expect(traveller.bags.length).toBe(1);
+    expect(traveller.bags[0]).toBe(bag);
+  });
+});
