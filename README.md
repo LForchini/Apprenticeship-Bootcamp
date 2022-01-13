43d127adad681413d8910273d152155b81f3ad7a
LForchini <leonardo.forchini@gmail.com>
2022-01-13 09:47:39 +0000

    Cleaned branch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Apprenticeship-Bootcamp
+
+This repo is structured so that each branch either represents a day or work, or a multi-day project.
--- Week 1/Day 3/AirportTS/tests/Airport.test.ts
+++
@@ -1,8 +0,0 @@
-import Airport from "../src/Airport";
-
-describe("Airport", () => {
-  test("is created", () => {
-    const airport = new Airport("Heathrow");
-    expect(airport.name).toBe("Heathrow");
-  });
-});
