2b3db5e7f3cc40bc65abd6eef95b8b3df848c137
LForchini <leonardo.forchini@gmail.com>
2022-01-13 14:37:12 +0000

    Added basic MenuItem model test

--- tests/MenuItem.test.ts
+++ tests/MenuItem.test.ts
@@ -6,7 +6,15 @@ describe("MenuItem", () => {
     await sequelize.sync({ force: true });
   });
 
-  it.todo("can be created");
+  it("can be created", async () => {
+    const menuItem: MenuItem = new MenuItem({
+      name: "Name",
+      price: 20,
+    });
+    await menuItem.save();
 
-  it.todo("can have multiple instances");
+    expect(menuItem.name).toBe("Name");
+    expect(menuItem.price).toBe(20);
+    expect(menuItem.id).not.toBe(null);
+  });
 });
