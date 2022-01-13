c9455f43b857685f5bda55d906db2dcfaea918dd
LForchini <leonardo.forchini@gmail.com>
2022-01-13 11:45:11 +0000

    Added other models

--- src/models/Menu.model.ts
+++ src/models/Menu.model.ts
@@ -3,13 +3,32 @@ import {
   Column,
   Model,
   DataType,
+  PrimaryKey,
+  AutoIncrement,
+  HasMany,
+  BelongsTo,
   ForeignKey,
 } from "sequelize-typescript";
 import Restaurant from "./Restaurant.model";
+import MenuItem from "./MenuItem.model";
 
 @Table
 export default class Menu extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
+
   @ForeignKey(() => Restaurant)
   @Column(DataType.INTEGER)
-  restaurant_id: number = 0;
+  restaurantId!: number;
+
+  @BelongsTo(() => Restaurant)
+  restaurant_id!: Restaurant;
+
+  @Column(DataType.TEXT)
+  title!: string;
+
+  @HasMany(() => MenuItem)
+  items!: MenuItem[];
 }
