28d72a53f425f47bce111866e6478da6322b7e5a
LForchini <leonardo.forchini@gmail.com>
2022-01-13 09:43:34 +0000

    Cleaned branch

--- projects/ScooterHire/src/User.ts
+++
@@ -1,9 +0,0 @@
-export default abstract class User {
-  name: string;
-  address: string;
-
-  constructor(name: string, address: string) {
-    this.name = name;
-    this.address = address;
-  }
-}
