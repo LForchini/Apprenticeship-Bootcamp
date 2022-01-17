558b75da6683a39cc3d35e7e8b211a21facd6a68
LForchini <leonardo.forchini@gmail.com>
2022-01-17 10:43:31 +0000

    Added basic endpoints

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
 
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server started listening at http://localhost:${PORT}`);
+});
+
+app.get("/now", (req: Request, res: Response) => {
+  const date: Date = new Date();
+  res.send(date);
+});
+
+app.get("/flipcoin", (req: Request, res: Response) => {
+  res.send(Math.random() > 0.5 ? "heads" : "tails");
+});
+
+app.get("/restaurants", (req: Request, res: Response) => {
+  // ToDo
 });
